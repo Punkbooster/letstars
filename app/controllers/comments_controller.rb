5f1e6c6a07d4fe646bd1d66858fee1ff8903f027
Punkbooster <2morrow2late@rambler.ru>
2015-04-26 09:52:59 +0200

    Add edit to comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,5 @@
 class CommentsController < ApplicationController
 
-	class CommentsController < ApplicationController
-
 	def create
 		@post = Post.find(params[:post_id])
 		@comment = @post.comments.create(params[:comment].permit(:comment))
@@ -37,5 +35,4 @@ class CommentsController < ApplicationController
 		@comment.destroy
 		redirect_to post_path(@post)
 	end
-
 end
