f8db00237d2211682a7ce71e21751a7b989c4676
Punkbooster <2morrow2late@rambler.ru>
2015-04-26 09:47:27 +0200

    Add blog with comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150419120219) do
+ActiveRecord::Schema.define(version: 20150426074615) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "comment"
+    t.integer  "post_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "contests", force: :cascade do |t|
     t.string   "image"
@@ -23,6 +34,13 @@ ActiveRecord::Schema.define(version: 20150419120219) do
   end
 
   add_index "contests", ["track_id"], name: "index_contests_on_track_id"
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title_blog"
+    t.text     "content_blog"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
