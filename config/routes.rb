e3a24e7c642b6444bcd82298d3a154d349ee9e24
meditate <aroundofyou@gmail.com>
2015-04-26 14:16:07 +0200

    Styling (add footer)

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   get 'home/index'
 
   devise_for :users
+
   root "home#index"
   resources :contests
   resources :profiles
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
