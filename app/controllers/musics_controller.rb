101af482a2980eed858c28ecb3be49d7575e4da2
Punkbooster <2morrow2late@rambler.ru>
2015-04-30 19:54:48 +0200

    Add musics views

---
+++ app/controllers/musics_controller.rb
@@ -0,0 +1,79 @@
+class MusicsController < ApplicationController
+	before_action :set_music, only: [:show, :edit, :update, :destroy]
+
+
+
+  def show
+  end
+
+  # GET /musics/new
+  def new
+  	@music = Music.new
+  end
+
+  # GET /musics/1/edit
+  def edit
+  end
+
+  # POST /musics
+  # POST /musics.json
+  def create
+  	@music = Music.new(music_params)
+
+  	respond_to do |format|
+  		if @music.save
+  			format.html { redirect_to @music, notice: 'Music was successfully created.' }
+  			format.json { render :show, status: :created, location: @music }
+  		else
+  			format.html { render :new }
+  			format.json { render json: @music.errors, status: :unprocessable_entity }
+  		end
+  	end
+  end
+
+  # PATCH/PUT /musics/1
+  # PATCH/PUT /musics/1.json
+  def update
+  	respond_to do |format|
+  		if @music.update(music_params)
+  			format.html { redirect_to @music, notice: 'Music was successfully updated.' }
+  			format.json { render :show, status: :ok, location: @music }
+  		else
+  			format.html { render :edit }
+  			format.json { render json: @music.errors, status: :unprocessable_entity }
+  		end
+  	end
+  end
+
+  # DELETE /musics/1
+  # DELETE /musics/1.json
+  def destroy
+  	@music.destroy
+  	respond_to do |format|
+  		format.html { redirect_to musics_url, notice: 'Music was successfully destroyed.' }
+  		format.json { head :no_content }
+  	end
+
+  	def audio_download
+  		@music = Music.find(params[:id])
+  		file_path = @music.audio_file_name
+  		if !file_path.nil?
+  			send_file "#{Rails.root}/public/system/audios/#{@music.id}/original/#{file_path}", :x_sendfile => true
+  		else
+  			redirect_to musics_url
+  		end
+  	end  
+
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_music
+    	@music = Music.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def music_params
+    	params.require(:music).permit(:audio)
+    end
+end
