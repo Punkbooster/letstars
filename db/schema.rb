77ad822d55bd17902b5ab535f697db5b0ead86b4
meditate <aroundofyou@gmail.com>
2015-05-07 11:08:37 +0200

    -styling a bit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150504083549) do
+ActiveRecord::Schema.define(version: 20150504152148) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "comment"
