6e616218800c06065f6e49b26bb34b2e7ae9937c
meditate <aroundofyou@gmail.com>
2015-06-21 02:03:01 +0300

    -add contest_end validation for user voting

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,8 @@ class User < ActiveRecord::Base
   	contest.musics.each do |music|
   		if self.voted_for?(music)
   			return false
+      elsif Date.today>contest.contest_end
+        return false
 		end
   	end
 
