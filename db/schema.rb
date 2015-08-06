f6620a00a9e913d92cf400de639e04a640776a43
Punkbooster <2morrow2late@rambler.ru>
2015-08-06 21:25:07 +0200

    Change text on image in contests

--- db/schema.rb
+++ db/schema.rb
@@ -28,12 +28,15 @@ ActiveRecord::Schema.define(version: 20150805112227) do
     t.string   "image"
     t.string   "title"
     t.text     "description"
+    t.integer  "track_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.datetime "contest_end"
     t.datetime "voting_end"
     t.string   "code"
   end
+
+  add_index "contests", ["track_id"], name: "index_contests_on_track_id"
 
   create_table "musics", force: :cascade do |t|
     t.string   "audio"
